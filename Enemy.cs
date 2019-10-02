1efad5213e20e908c7d87ecdf179c23f4d1f74b3
PartumGames <37095216+PartumGames@users.noreply.github.com>
2019-10-02 10:58:36 -0500

    Add files via upload
    Initial Commit

---
+++ Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// This is a test script to tell if your weapons are actually shooting/hitting a target. 
+    /// Play this script on a gameobject and make sure to give it a tag of Enemy
+    /// </summary>
+
+
+   public void TakeDamage(float _amnt)
+    {
+        Debug.Log("Enemy was hit with " + _amnt.ToString() + " of damage");
+    }
+}
